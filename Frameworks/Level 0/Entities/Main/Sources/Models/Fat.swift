5357e867db2a0532df1cfa6b5e89fa0ccaa409f8
Igor Mischenko <lagrave.dev@gmail.com>
2022-07-09 13:39:21 +0300

    added product and fat entities

---
+++ Frameworks/Level 0/Entities/Main/Sources/Models/Fat.swift
@@ -0,0 +1,7 @@
+public struct Fat: Hashable {
+
+    public let common: Float
+    public let saturated: Float
+    public let unsaturated: Float
+
+}
---
+++ Frameworks/Level 0/Entities/Main/Sources/Models/Product.swift
@@ -0,0 +1,17 @@
+public struct Product: Hashable {
+
+    public let id: Int
+    public let title: String
+    public let calories: Int
+    public let carbs: Float
+    public let protein: Float
+    public let fat: Fat
+    public let fiber: Float
+    public let cholesterol: Float
+    public let sugar: Float
+    public let sodium: Float
+    public let potassium: Float
+    public let gramsPerServing: Float
+    public let pcsText: String
+
+}
